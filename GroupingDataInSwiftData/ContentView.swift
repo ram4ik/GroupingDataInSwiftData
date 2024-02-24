3031b929773a434f82e25c46b1c4198b72f7f52a
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-24 10:05:23 +0200

    init

--- GroupingDataInSwiftData/ContentView.swift
+++ GroupingDataInSwiftData/ContentView.swift
@@ -6,16 +6,37 @@
 //
 
 import SwiftUI
+import SwiftData
+
+@Model
+class Movie {
+    
+    var name: String
+    var genre: String
+    
+    init(name: String, genre: String) {
+        self.name = name
+        self.genre = genre
+    }
+}
 
 struct ContentView: View {
+    
+    @Environment(\.modelContext) private var context
+    @Query private var movies: [Movie]
+    
+    @State private var name: String = ""
+    @State private var genre: String = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            TextField("Enter name", text: $name)
+            TextField("Enter genre", text: $genre)
+            Button("Save") {
+                let movie = Movie(name: name, genre: genre)
+                context.insert(movie)
+            }
         }
-        .padding()
     }
 }
 
