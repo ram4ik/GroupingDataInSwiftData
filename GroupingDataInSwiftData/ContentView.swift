239fa35a72a6199f0a60b4118e25c0cef3332205
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-02-24 09:57:01 +0200

    Initial Commit

---
+++ GroupingDataInSwiftData/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroupingDataInSwiftData
+//
+//  Created by test on 24.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroupingDataInSwiftData/GroupingDataInSwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupingDataInSwiftDataApp.swift
+//  GroupingDataInSwiftData
+//
+//  Created by test on 24.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct GroupingDataInSwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
